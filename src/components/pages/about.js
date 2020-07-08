bfc5e45654f11957b2dfec6419146f61ea4ed0b1
Debu07 <mehtavedant7498@gmail.com>
2020-07-08 18:57:42 +0100

    react todo app

---
+++ src/components/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function about() {
+    return (
+        <React.Fragment>
+            <h1> About</h1>
+            <p> This is the TodoList app v1.0.0 .
+                It is part of react learning.
+            </p>
+            <p> by- Vedant Mehta</p>
+            <a href="https://github.com/Debu07">(Github)</a>
+            </React.Fragment>
+    )
+}
